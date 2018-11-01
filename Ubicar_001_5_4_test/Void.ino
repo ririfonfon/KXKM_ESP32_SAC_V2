1d5c8d8c75db73bfe7768ce382023df626f27d48
RIRI <ririfonfon10@gmail.com>
2018-11-01 12:00:00 +0100

    menage

--- Ubicar_001_5_4_test/Void.ino
+++
@@ -1,7 +0,0 @@
-void Black() {
-  for (int i = 0 ; i < NUM_LEDS_PER_STRIP ; i++) {
-    strands[0]->pixels[i] = pixelFromRGB(0, 0, 0);
-    strands[1]->pixels[i] = pixelFromRGB(0, 0, 0);
-  }//for i
-}
-
