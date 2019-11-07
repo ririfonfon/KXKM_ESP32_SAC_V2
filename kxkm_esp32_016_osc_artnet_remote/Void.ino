521111b8a39ab27e2bf08173aab9773c1806c265
RIRI <ririfonfon10@gmail.com>
2019-11-07 11:28:55 +0100

    clean black

--- kxkm_esp32_016_osc_artnet_remote/Void.ino
+++ kxkm_esp32_016_osc_artnet_remote/Void.ino
@@ -1,5 +1,5 @@
 void Black() {
-  for (int i = 0 ; i < NUM_LEDS_PER_STRIP ; i++) {
+  for (int i = 0 ; i < NUM_LEDS_PER_STRIP_MAX ; i++) {
     strands[1]->pixels[i] = pixelFromRGBW(0, 0, 0, 0);
   }//for i
 }
