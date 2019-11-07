254d48b08b555b0502afd9a5a73ba20608a0b7be
RIRI <ririfonfon10@gmail.com>
2019-11-07 16:21:04 +0100

    add zo_pi_n to black

--- kxkm_esp32_016_osc_artnet_remote/Void.ino
+++ kxkm_esp32_016_osc_artnet_remote/Void.ino
@@ -1,5 +1,9 @@
 void Black() {
   for (int i = 0 ; i < NUM_LEDS_PER_STRIP_MAX ; i++) {
     strands[1]->pixels[i] = pixelFromRGBW(0, 0, 0, 0);
+    zo_pi_n_1_r[i] = 0;
+    zo_pi_n_1_g[i] = 0;
+    zo_pi_n_1_b[i] = 0;
+    zo_pi_n_1_w[i] = 0;
   }//for i
 }
