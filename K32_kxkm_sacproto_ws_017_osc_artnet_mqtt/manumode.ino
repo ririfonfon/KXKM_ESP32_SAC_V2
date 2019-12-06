8f1f33431ef3324aaf3177292d8c309bd26ba01c
RIRI <ririfonfon10@gmail.com>
2019-12-06 13:07:34 +0100

    add mirror

--- K32_kxkm_sacproto_ws_017_osc_artnet_mqtt/manumode.ino
+++ K32_kxkm_sacproto_ws_017_osc_artnet_mqtt/manumode.ino
@@ -1,11 +1,11 @@
 int MEM[5][LULU_PATCHSIZE] = {
-  {255    , 255, 255, 255, 255, 0      , 0        , 0       , 11      , 30        , 0      , 0      , 0      , 0      , 0         , 0    , 0   , 0   },  // str white
-  {127    , 255, 0  , 0  , 0  , 0      , 0        , 0       , 0       , 0         , 0      , 0      , 0      , 0      , 0         , 0    , 0   , 0   },  // Red
-  {127    , 0  , 255, 0  , 0  , 0      , 0        , 0       , 0       , 0         , 0      , 0      , 0      , 0      , 0         , 0    , 0   , 0   },  // Green
-  {127    , 0  , 0  , 255, 0  , 0      , 0        , 0       , 0       , 0         , 0      , 0      , 0      , 0      , 0         , 0    , 0   , 0   },  // Blue
-  {127    , 255, 255, 0  , 0  , 0      , 0        , 0       , 0       , 0         , 0      , 0      , 0      , 0      , 0         , 0    , 0   , 0   }   // Yellow
+  {255    , 255, 255, 255, 255, 0      , 0        , 0       , 11      , 30        , 0      , 0      , 0      , 0      , 0         , 0          , 255  , 0   , 0   },  // str white
+  {127    , 255, 0  , 0  , 0  , 0      , 0        , 0       , 0       , 0         , 0      , 0      , 0      , 0      , 0         , 0          , 255  , 0   , 0   },  // Red
+  {127    , 0  , 255, 0  , 0  , 0      , 0        , 0       , 0       , 0         , 0      , 0      , 0      , 0      , 0         , 0          , 255  , 0   , 0   },  // Green
+  {127    , 0  , 0  , 255, 0  , 0      , 0        , 0       , 0       , 0         , 0      , 0      , 0      , 0      , 0         , 0          , 255  , 0   , 0   },  // Blue
+  {127    , 255, 255, 0  , 0  , 0      , 0        , 0       , 0       , 0         , 0      , 0      , 0      , 0      , 0         , 0          , 255  , 0   , 0   }   // Yellow
 };
-//{master , r  , g  , b  , w  ,pix mod , pix long , pix_pos , str_mod , str_speed , r_fond , g_fond , b_fond , w_fond , color_mod , zoom , pw1 , pw2 }
+//{master , r  , g  , b  , w  ,pix mod , pix long , pix_pos , str_mod , str_speed , r_fond , g_fond , b_fond , w_fond , color_mod , mirror_mod , zoom , pw1 , pw2 }
 uint8_t fakeframe[adr + LULU_PATCHSIZE];
 
 void manu_frame(int mem) {
