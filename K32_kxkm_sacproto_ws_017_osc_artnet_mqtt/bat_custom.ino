bb2123ed6297cfbec8f60d3ff071ded51121bc8e
RIRI <ririfonfon10@gmail.com>
2019-12-06 19:09:21 +0100

    Update bat_custom.ino

--- K32_kxkm_sacproto_ws_017_osc_artnet_mqtt/bat_custom.ino
+++ K32_kxkm_sacproto_ws_017_osc_artnet_mqtt/bat_custom.ino
@@ -1,11 +1,11 @@
 void bat_custom() {
-  int Ulow = 21700;
-  int U1 = 23100;
-  int U2 = 23500;
+  int Ulow = 21700;// rouge clignote
+  int U1 = 23100;  // rouge low
+  int U2 = 23500;  // orange
   int U3 = 24500;
   int U4 = 26500;
   int U5 = 28000;
-  int Umax = 29400;
+  int Umax = 29400; //vert
 
   k32->system->stm32->custom( Ulow, U1,  U2, U3, U4, U5, Umax);// set CUSTOM profile
 
