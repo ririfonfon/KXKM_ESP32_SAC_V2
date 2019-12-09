97c5c1c09c95a8854f9b2159e2123799736c7bb0
RIRI <ririfonfon10@gmail.com>
2019-12-09 15:37:31 +0100

    tous type pince_fluo_barre_etc...

---
+++ K32_kxkm_esp32_019_osc_artnet_mqtt/PWM.ino
@@ -0,0 +1,26 @@
+int freq = 40000;
+int resol = 8;
+
+void ledSetup() {
+  //// Setup LED for Testing
+  ledcSetup(1, freq, resol);
+  ledcSetup(2, freq, resol);
+  ledcAttachPin(17, 1);
+  ledcAttachPin(16, 2);
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
+}
+
+//// Setup PWM for LED(s)
+void leds() {
+  channelOne();
+  channelTwo();
+}
+
+void channelOne() {
+  ledcWrite(1, ledChannelOne);
+}
+
+void channelTwo() {
+  ledcWrite(2, ledChannelTwo);
+}
