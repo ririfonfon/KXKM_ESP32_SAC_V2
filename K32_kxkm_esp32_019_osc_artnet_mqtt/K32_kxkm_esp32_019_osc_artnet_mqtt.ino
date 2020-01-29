8d1c2abfbe489b8c091f0cf46ce6086efa600871
maigre <thomas.bohl@gmail.com>
2020-01-29 14:37:29 +0100

    define vs Preferences

--- K32_kxkm_esp32_019_osc_artnet_mqtt/K32_kxkm_esp32_019_osc_artnet_mqtt.ino
+++ K32_kxkm_esp32_019_osc_artnet_mqtt/K32_kxkm_esp32_019_osc_artnet_mqtt.ino
@@ -1,13 +1,13 @@
 #include "K32.h"
 
-#define LULU_VER  42
+#define LULU_VER  43
 
 /////////////////////////////////////////ID/////////////////////////////////////////
- //#define K32_SET_NODEID        63 // board unique id    (necessary first time only)
+ #define K32_SET_NODEID        61 // board unique id    (necessary first time only)
  //#define K32_SET_HWREVISION    2  // board HW revision  (necessary first time only)
 
- //#define RUBAN_TYPE LED_SK6812W_V1  // LED_WS2812_V1  LED_WS2812B_V1  LED_WS2812B_V2  LED_WS2812B_V3  LED_WS2813_V1  LED_WS2813_V2   LED_WS2813_V3  LED_WS2813_V4  LED_SK6812_V1  LED_SK6812W_V1,
- //#define LULU_ID   4                // permet de calculer l'adresse DMX
+ #define RUBAN_TYPE LED_SK6812W_V1  // LED_WS2812_V1  LED_WS2812B_V1  LED_WS2812B_V2  LED_WS2812B_V3  LED_WS2813_V1  LED_WS2813_V2   LED_WS2813_V3  LED_WS2813_V4  LED_SK6812_V1  LED_SK6812W_V1,
+ #define LULU_ID   19                // permet de calculer l'adresse DMX
  #define LULU_TYPE 5                // 1="Sac" 2="Barre" 3="Pince" 4="Fluo" 5="Flex" 6="H&S" 7="Phone"
  //#define LULU_UNI  0                // Univers DM
 
@@ -276,9 +276,9 @@ void loop() {
   eff_modulo();
 
   // Click on ESP
-  if (k32->system->stm32->clicked()) {
+  /*if (k32->system->stm32->clicked()) {
     manu_frame(++manu_counter);
     LOG("clicked");
-  }
+  }*/
 
 }//loop
--- K32_kxkm_esp32_019_osc_artnet_mqtt/k32_settings.ino
+++ K32_kxkm_esp32_019_osc_artnet_mqtt/k32_settings.ino
@@ -4,34 +4,53 @@ void k32_settings() {
   // Save to EEPROM if DEFINE
 #ifdef LULU_ID
   k32->system->preferences.putUInt("LULU_id", LULU_ID);
-#endif
-#ifdef LULU_TYPE
-  k32->system->preferences.putUInt("LULU_type", LULU_TYPE);
-#endif
-#ifdef LULU_UNI
-  k32->system->preferences.putUInt("LULU_uni", LULU_UNI);
-#endif
-#ifdef RUBAN_TYPE
-  k32->system->preferences.putUInt("RUBAN_type", RUBAN_TYPE);
-#endif
-#ifdef R_TYPE
-  k32->system->preferences.putString("R_type", R_TYPE);
-#endif
-#ifdef L_TYPE
-  k32->system->preferences.putString("L_type", L_TYPE);
-#endif
-#ifdef NUM_LEDS_PER_STRIP_MAX
-  k32->system->preferences.putUInt("NUM_leds", NUM_LEDS_PER_STRIP_MAX);
+  LULU_id = LULU_ID;
+#else
+  LULU_id = k32->system->preferences.getUInt("LULU_id", 1);
 #endif
 
-  // Load from EEPROM
-  LULU_id = k32->system->preferences.getUInt("LULU_id", 1);
+#ifdef LULU_TYPE
+  k32->system->preferences.putUInt("LULU_type", LULU_TYPE);
+  LULU_type = LULU_TYPE;
+#else
   LULU_type = k32->system->preferences.getUInt("LULU_type", 5);
+#endif
+
+#ifdef LULU_UNI
+  k32->system->preferences.putUInt("LULU_uni", LULU_UNI);
+  LULU_uni = LULU_UNI;
+#else
   LULU_uni = k32->system->preferences.getUInt("LULU_uni", 0);
+#endif
+
+#ifdef RUBAN_TYPE
+  k32->system->preferences.putUInt("RUBAN_type", RUBAN_TYPE);
+  RUBAN_type = RUBAN_TYPE;
+#else
   RUBAN_type = k32->system->preferences.getUInt("RUBAN_type", LED_SK6812W_V1);
+#endif
+
+#ifdef R_TYPE
+  k32->system->preferences.putString("R_type", R_TYPE);
+  R_type = R_TYPE;
+#else
   R_type = k32->system->preferences.getString("R_type", "_SK");
+#endif
+
+#ifdef L_TYPE
+  k32->system->preferences.putString("L_type", L_TYPE);
+  L_type = L_TYPE;
+#else
   L_type = k32->system->preferences.getString("L_type", "Flex");
+#endif
+
+#ifdef NUM_LEDS_PER_STRIP_MAX
+  k32->system->preferences.putUInt("NUM_leds", NUM_LEDS_PER_STRIP_MAX);
+  NUM_LEDS_PER_STRIP_max = NUM_LEDS_PER_STRIP_MAX;
+#else
   NUM_LEDS_PER_STRIP_max = k32->system->preferences.getUInt("NUM_leds", 186);
+#endif
+  
 
   // Calculate adr // channels
   adr = (1 + (LULU_id - 1) * (LULU_PATCHSIZE));
